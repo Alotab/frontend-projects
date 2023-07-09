65e0f86bcf8f32ce1edc94cd32c72920d503f9a0
alotab <alotab@yahoo.com>
2023-07-09 23:00:30 +0000

    update files

--- BlogApp/js/script.js
+++ BlogApp/js/script.js
@@ -12,6 +12,79 @@ const navCloseToggle = document.querySelector('.mobile-nav-toggle-close');
 const elSearcIcon = document.querySelector('.search-icon')
 const elSearchMenu = document.querySelector('.middle-colume-search')
 
+// variables for chatbot 
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+const chatbox = document.querySelector(".chatbox");
+
+
+let userMessenger;
+let API_KEY = "";
+
+const createChatLi = (message, className) => {
+    // create a chat <li> element with passed message and className
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", className);
+
+    let chatContent = className === "outgoing" ? `<p></p><span class="material-symbols-outlined">
+    <img src="images/user.png" alt="">
+</span>` : ` <span class="material-symbols-outlined">
+    <img src="images/chatbot.png" alt=""></span><p></p>`;
+    chatLi.innerHTML = chatContent;
+    chatLi.querySelector("p").textContent = message;
+    return chatLi;
+}
+
+const generateResponse = (incomingChatLi) => {
+    const API_URL = "https://api.openai.com/v1/chat/completions"
+    const messageElement = incomingChatLi.querySelector("p")
+
+    const requestOptions = {
+        method :"POST",
+        Headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": "user", "content": userMessenger}]
+        })
+    }  
+    
+    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+        // console.log(data);
+        messageElement.textContent = data.choices[0].message.content;
+    }).catch((error) => {
+        // console.log(error);
+        messageElement.textContent = "Oops! Something went wrong. Please try again."
+    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+}
+    
+   
+
+sendChatBtn.addEventListener('click', ()=> {
+    userMessenger = chatInput.value.trim()
+    if(!userMessenger) return;
+    chatInput.value = "";
+
+
+    // append user's message to the chatbox
+    chatbox.appendChild(createChatLi(userMessenger, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+
+
+    setTimeout(() => {
+        // chatbox.appendChild(createChatLi("...", "incoming"));
+
+        const incomingChatLi = createChatLi("...", "incoming")
+        chatbox.appendChild(incomingChatLi);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+        generateResponse(incomingChatLi);
+    }, 600);
+    
+
+})
+
 
 
 // prevent and enable scrolling when hambuger menu is clicked
