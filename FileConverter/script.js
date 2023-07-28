b07ba236d6f86a8a16924698031b326579ba1ddc
alotab <alotab@yahoo.com>
2023-07-28 06:34:59 +0000

    update files

--- FileConverter/script.js
+++ FileConverter/script.js
@@ -3,8 +3,6 @@ const dropEl = document.querySelector(".drop-down");
 const dropdownEl = document.querySelector('.drop-down-list');
 
 
-
-
 dropEl.addEventListener('click', () => {
     console.log('hi');
     dropdownEl.classList.add('active');
@@ -25,24 +23,35 @@ dropEl.addEventListener('click', () => {
 // const realsizekb = filesize / 1024;
 
 
-
-////online 
 const fileIn = document.getElementById('file-input');
 const selectedFiles = document.getElementById('files-list')
 
-fileIn.onchange = () => {
+let fileList = [];
+
+function handleFilesSelected(){
     const selectFiles = [...fileIn.files];
     for (const f of selectFiles){
-
+        fileList.push(f);
+        // console.log(fileList);
         const li = document.createElement('li');
         li.innerHTML =    `
                         <p>${f.name}</p>
-                        <div class="scroll-pick"></div>
-                        <div class="file-size">${f.size}<span>KB</span></div>
-                        <div class="close-button"><i class="ri-close-line"></i></div>
+                        <div class="scroll-pick">
+                            <div class="spining">
+                                <!-- <p class="reading">Reading</p> -->
+                                <i class="ri-refresh-line"></i>
+                            </div>
+                            <!-- <div class="status">Finished</div> -->
+                        </div>
+                        <div class="file-size">${f.size}<span>kb</span></div>
+                        <div class="close-button">
+                            <i class="ri-close-line"></i>
+                            <div class="download-link">
+                                <a href="">download</a>
+                                </div>
+                        </div>
                         `;
         selectedFiles.appendChild(li);
-
 
         // Delete any of the Uploaded files
         const deleteButton = li.querySelector('.close-button');
@@ -51,4 +60,57 @@ fileIn.onchange = () => {
           li.remove();
         });
     };
-};
+}
+
+
+// Convert any File 
+function convertFiles(){
+    // Get the file types of the files in the file list
+    const fileTypes = fileList.map(file => file.type);
+
+    // Check if all files are PDF files
+    if(fileTypes.every(type => type === 'application/pdf')) {
+        // Convert the files to CSV files
+        const csvFiles = fileList.map(file => convertPDFToCSV(file));
+        console.log(csvFiles);
+        // Download the CSV files
+        // csvFiles.forEach(csvFile => downloadCSVFile(csvFile));
+     
+    } else {
+        console.log('The files you entered are not PDF ....');
+    }
+    
+}
+
+document.getElementById('file-input').addEventListener('change', handleFilesSelected, false);
+document.querySelector('.convert').addEventListener('click', convertFiles);
+
+
+// Convert PDF -> CSV
+async function convertPDFToCSV(file){
+    const csvFile = new Blob([file], {'type': 'application/csv'});
+    // console.log(csvFile);
+   
+    // Download the CSV file
+    return csvFile;
+}
+
+
+function downloadCSVFile(csvFile) {
+    // get filename
+    const filename = csvFile.name;
+
+    // Create a link to download the CSVFile
+    const donwloadLinkParent = document.querySelector('.download-link');
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(csvFile);
+    link.download = filename;
+
+    donwloadLinkParent.appendChild(link);
+
+    link.click();
+}
+
+
+  
+  
