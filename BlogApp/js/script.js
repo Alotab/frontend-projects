e5549b48114295623ef3c0f70723691b325f52b4
alotab <alotab@yahoo.com>
2023-08-06 23:16:39 +0000

    update files

--- BlogApp/js/script.js
+++ BlogApp/js/script.js
@@ -80,56 +80,179 @@ function enableScroll() {
 
 
 /*=============== EXTERNAL LINK SCREEN  ===============*/
-const detailImage = document.querySelector(".post-image");
+// const detailImage = document.querySelector(".post-image");
 const sharelinkOne = document.querySelector(".share-link");
 const postdetailOne = document.querySelector(".post-main-detail");
 const externalOne = document.querySelector(".external-share-container");
 
-
-// Create a function to check if the div is reached
-function isDivReached() {
-  // Get the current scroll position
-  const scrollPosition = window.scrollY;
-
-  // Check if the scroll position is greater than or equal to the top of the div
-  return scrollPosition >= externalOne.offsetTop - 750;
+function shareLinkfun(entries){
+    const bottom = window.innerHeight - 30;
+    if (entries[0].intersectionRect.bottom >= bottom) {
+        console.log('Element is visible');
+    }
 }
+
+const options = {
+    // root: null,
+   rootMargin: "0px 0px 30px 0px",
+//    rootMargin: "30px",
+   threshold: 0,
+}
+const shareLinkObserver =  new IntersectionObserver(shareLinkfun, options);
+// shareLinkObserver.observe(sharelinkOne);
+
+
+
+// const shareLinkScroll = new IntersectionObserver((entries, observe) => {
+//     // const bottom = window.innerHeight - 50;
+//     // if (entries[0].intersectionRect.bottom <= bottom) {
+//     //     // console.log('Element is bottom');
+
+//     // }
+//     const bottom = window.innerHeight - 30;
+    
+//     if(entries[0].intersectionRect.bottom >= bottom && sharelinkOne.getBoundingClientRect.top <= window.innerHeight / 2){
+//         console.log('hi');
+//         shareLinkScroll.unobserve(sharelinkOne);
+//     }
+// }, {
+//     root: null,
+//     threshold: 0
+// });
+
+// shareLinkScroll.observe(sharelinkOne);
+
+const observer = new IntersectionObserver((entries) => {
+    if (entries[0].isIntersecting) {
+    //   sharelinkOne.classList.add('share-java');
+    //   sharelinkOne.classList.remove('share-scroll');
+     
+    
+      console.log('hi');
+      
+    } else {
+        // sharelinkOne.classList.add('share-scroll');
+    }
+  }, {
+    // rootMargin: '-750px 0px 0px 0px',
+    // rootMargin: "0px 0px 0px 0px",
+    threshold: 0,
+  });
+  
+//   observer.observe(sharelinkOne);
+
+
+//   const sharelinkOne = document.querySelector('#your-div-id');
+  let isDivReached = false;
+  
+  const checkDivPosition = () => {
+    const divTop = externalOne.offsetTop - 750;
+    const divBottom = externalOne.offsetTop + externalOne.offsetHeight - 30;
+    const scrollPosition = window.scrollY;
+    const windowHeight = window.innerHeight;
+  
+    if (scrollPosition >= divTop && scrollPosition <= divBottom && !isDivReached) {
+    //   sharelinkOne.classList.add('share-java');
+      isDivReached = true;
+    } else if ((scrollPosition < divTop || scrollPosition > divBottom) && isDivReached) {
+    //   sharelinkOne.classList.remove('share-java');
+    //   isDivReached = false;
+    }else {
+        sharelinkOne.classList.remove('share-java');
+    }
+  
+    // if (scrollPosition + windowHeight >= divBottom && !isDivReached) {
+    //   sharelinkOne.classList.add('share-scroll');
+    //   isDivReached = true;
+    // } else if (scrollPosition + windowHeight < divBottom && isDivReached) {
+    //   sharelinkOne.classList.remove('share-scroll');
+    //   isDivReached = false;
+    // }
+  };
+  
+  window.addEventListener('scroll', checkDivPosition);
+  
+ 
+
+
+
+
+
+
+
+/// Create a function to check if the div is reached
+// function isDivReached() {
+//   /// Get the current scroll position
+//   const scrollPosition = window.scrollY;
+
+//   /// Check if the scroll position is greater than or equal to the top of the div
+//   return scrollPosition >= externalOne.offsetTop - 750;
+// }
 
 // Add an event listener to the window scroll event
-window.addEventListener('scroll', function (e) {
-    e.preventDefault();
-  // Check if the div is reached
-  if (isDivReached()) {
-    sharelinkOne.classList.add('share-java');
-  }
-  else {
-    sharelinkOne.classList.remove('share-java');
-  }
-});
+// window.addEventListener('scroll', function (e) {
+//     e.preventDefault();
+//   // Check if the div is reached
+//   if (isDivReached()) {
+//     sharelinkOne.classList.add('share-java');
+//   }
+//   else {
+//     sharelinkOne.classList.remove('share-java');
+//   }
+// });
 
-function Reached() {
-    // Get the current scroll position
+const socialBox = () => {
     const scrollPosition = window.scrollY;
 
-    // Check if the scroll position is greater than or equal to the top of the div
-    return scrollPosition <= externalOne.offsetTop;
-}
 
-// prevent scroll event
-window.addEventListener('scroll', function (e) {
-    if(Reached){
+    if(scrollPosition <= externalOne.offsetTop || scrollPosition <= externalOne.offsetTop - 750){
+        // sharelinkOne.classList.add("share-scroll");
+        sharelinkOne.classList.add('share-java');
+    } else {
+        sharelinkOne.classList.remove('share-java');
+    }
+
+    if(scrollPosition <= externalOne.offsetTop){
         sharelinkOne.classList.add("share-scroll");
     }
-});
-window.addEventListener('scroll', function (e) {
-    const scrollPosition = window.pageYOffset;
-    
-    if (scrollPosition <= externalOne.offsetTop - 798) {  //>= externalOne.offsetTop
+
+    if(window.scrollY <= externalOne.offsetTop - 705){
         sharelinkOne.classList.add('share-scroll');
     } else {
         sharelinkOne.classList.remove('share-scroll');
     }
-});
+}
+
+
+window.addEventListener('scroll', socialBox);
+
+
+// function Reached() {
+//     /// Get the current scroll position
+//     const scrollPosition = window.scrollY;
+
+//     /// Check if the scroll position is greater than or equal to the top of the div
+//     return scrollPosition <= externalOne.offsetTop;
+// }
+
+//// prevent scroll event
+// window.addEventListener('scroll', function (e) {
+//     if(Reached){
+//         sharelinkOne.classList.add("share-scroll");
+//     }
+// });
+// window.addEventListener('scroll', function (e) {
+//     const scrollPosition = window.pageYOffset;
+    
+//     if (scrollPosition <= externalOne.offsetTop - 798) { 
+//         sharelinkOne.classList.add('share-scroll');
+//     } else {
+//         sharelinkOne.classList.remove('share-scroll');
+//     }
+// });
+
+
+
 
 
 
@@ -158,92 +281,94 @@ const hidePopupWidget = document.querySelector('.kat');
 
 let showMes = false;
 // var showWidget = true;
-shareEl.addEventListener('click', () => {
-    socialContainerEl.classList.add('show-social-links');
-    likePop.classList.remove('alert-active');
-    
-    showMes = true;
-    if(showMes){
-        document.body.style.overflow = 'hidden';
+if(shareEl){
+    shareEl.addEventListener('click', () => {
+        socialContainerEl.classList.add('show-social-links');
+        likePop.classList.remove('alert-active');
         
-    }
-
-    document.addEventListener('click', (e) => {
-        if (!socialContainerEl.contains(e.target) && !shareEl.contains(e.target)) {
-            socialContainerEl.classList.remove('show-social-links');
-            document.body.style.overflow = 'auto';
-          
+        showMes = true;
+        if(showMes){
+            document.body.style.overflow = 'hidden';
+            
         }
-    });    
-});
-
-
-//show widget when mouseover icon
-const likeMouseOver = document.querySelector('.share-like');
-const commentMouseOver = document.querySelector('.share-comment');
-const bookmarkMouseOver = document.querySelector('.share-bookmark');
-const shareMouseOver = document.querySelector('.share-share');
-const likePop = document.querySelector('.pop-like');
-const commentPop = document.querySelector('.pop-comment');
-const bookmarkPop = document.querySelector('.pop-bookmark');
-const sharePop = document.querySelector('.pop-share');
-
-function showWidget() {
-    likeMouseOver.addEventListener('mouseover', () =>{
-
-        setTimeout(() => {
-            likePop.classList.add('alert-active');
-        }, 10);
-        likeMouseOver.addEventListener('mouseout', () =>{
-            likePop.classList.remove('alert-active');
-        })
+    
+        document.addEventListener('click', (e) => {
+            if (!socialContainerEl.contains(e.target) && !shareEl.contains(e.target)) {
+                socialContainerEl.classList.remove('show-social-links');
+                document.body.style.overflow = 'auto';
+              
+            }
+        });    
     });
-
-    commentMouseOver.addEventListener('mouseover', () =>{
-        setTimeout(() => {
-            commentPop.classList.add('alert-active');
-        }, 10);
-        commentMouseOver.addEventListener('mouseout', () =>{
-            commentPop.classList.remove('alert-active');
-        })
-    });
-
-    bookmarkMouseOver.addEventListener('mouseover', () =>{
-        setTimeout(() => {
-            bookmarkPop.classList.add('alert-active');
-        }, 10);
-        bookmarkMouseOver.addEventListener('mouseout', () =>{
-            bookmarkPop.classList.remove('alert-active');
-        })
-    });
-
-    shareMouseOver.addEventListener('mouseover', () =>{
-        setTimeout(() => {
-            sharePop.classList.add('alert-active');
-        }, 10);
-        shareMouseOver.addEventListener('mouseout', () =>{
-            sharePop.classList.remove('alert-active');
-        })
-    });
-};
-showWidget();
+}
 
 
-// copy permalink/url when user click permalink button
-const permalinkEl = document.querySelector('.permalink');
-const copyArticl = document.querySelector('.permalink-message');
+/////show widget when mouseover icon
+// const likeMouseOver = document.querySelector('.share-like');
+// const commentMouseOver = document.querySelector('.share-comment');
+// const bookmarkMouseOver = document.querySelector('.share-bookmark');
+// const shareMouseOver = document.querySelector('.share-share');
+// const likePop = document.querySelector('.pop-like');
+// const commentPop = document.querySelector('.pop-comment');
+// const bookmarkPop = document.querySelector('.pop-bookmark');
+// const sharePop = document.querySelector('.pop-share');
+
+// function showWidget() {
+//     likeMouseOver.addEventListener('mouseover', () =>{
+
+//         setTimeout(() => {
+//             likePop.classList.add('alert-active');
+//         }, 10);
+//         likeMouseOver.addEventListener('mouseout', () =>{
+//             likePop.classList.remove('alert-active');
+//         })
+//     });
+
+//     commentMouseOver.addEventListener('mouseover', () =>{
+//         setTimeout(() => {
+//             commentPop.classList.add('alert-active');
+//         }, 10);
+//         commentMouseOver.addEventListener('mouseout', () =>{
+//             commentPop.classList.remove('alert-active');
+//         })
+//     });
+
+//     bookmarkMouseOver.addEventListener('mouseover', () =>{
+//         setTimeout(() => {
+//             bookmarkPop.classList.add('alert-active');
+//         }, 10);
+//         bookmarkMouseOver.addEventListener('mouseout', () =>{
+//             bookmarkPop.classList.remove('alert-active');
+//         })
+//     });
+
+//     shareMouseOver.addEventListener('mouseover', () =>{
+//         setTimeout(() => {
+//             sharePop.classList.add('alert-active');
+//         }, 10);
+//         shareMouseOver.addEventListener('mouseout', () =>{
+//             sharePop.classList.remove('alert-active');
+//         })
+//     });
+// };
+// showWidget();
+
+
+// // copy permalink/url when user click permalink button
+// const permalinkEl = document.querySelector('.permalink');
+// const copyArticl = document.querySelector('.permalink-message');
 
 
 
-permalinkEl.addEventListener('click', (e) => {
-    copyArticl.classList.add('show-permalink-message');
-    e.preventDefault();
+// permalinkEl.addEventListener('click', (e) => {
+//     copyArticl.classList.add('show-permalink-message');
+//     e.preventDefault();
 
 
-    setTimeout(()=>{
-        copyArticl.classList.remove('show-permalink-message');
-    }, 1000)
-})
+//     setTimeout(()=>{
+//         copyArticl.classList.remove('show-permalink-message');
+//     }, 1000)
+// })
 
 
 
