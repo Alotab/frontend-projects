e13d11507b8787869544d803041d0ca7acd2505d
alotab <alotab@yahoo.com>
2023-11-26 00:04:14 +0000

    update files

--- crmboard/chartttsTuto.js
+++ crmboard/chartttsTuto.js
@@ -1,12 +1,13 @@
 
 
-const labels = Utils.months({count: 12});
+const labels = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
 const data = {
   labels: labels,
   datasets: [{
-    label: 'My First Dataset',
-    data: [20, 40, 30, 40, 35, 40, 45, 39, 54, 25, 30, 25],
-    fill: false,
+    label: 'My First Dataset', //
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: true, // True to fill the line chart
+    backgroundColor: '#9BD0F5', // background underneeath the line chart
     borderColor: 'rgb(75, 192, 192)',
     tension: 0.1
   }]
